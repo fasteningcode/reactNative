d8b821190f101649238d8336c2b15abadea56478
Aadhi <aadhi@fasteningcode.com>
2020-03-21 16:08:46 +1300

    destructuring navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,16 +2,21 @@ import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 import ComponentsScreen from "./ComponentsScreen";
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
   // console.log(props);
 
   return (
     <View>
       <Text style={styles.text}>Hello World always give me a great start</Text>
       <Button
-        onPress={() => props.navigation.navigate("Components")}
+        onPress={() => navigation.navigate("Components")}
         title="Go to Components Demo" />
-      <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
+
+      <Button
+        onPress={() => navigation.navigate('List')}
+        title="Go to List"
+      />
+      <TouchableOpacity onPress={() => navigation.navigate("List")}>
         <Text>Go to list Demo</Text>
         <Text>Go to list Demo</Text>
         <Text>Go to list Demo</Text>
