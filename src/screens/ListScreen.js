da23aef152a5ab4d29088d4a0b15afb19b26cddb
Aadhi <aadhi@fasteningcode.com>
2020-03-21 14:59:06 +1300

    List Screen - Horizontal and no scroll

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,25 +4,37 @@ import { FlatList } from 'react-native-gesture-handler';
 
 const ListScreen = () => {
     const friends = [
-        { name: 'friend #1', key: '1' },
-        { name: 'friend #2', key: '2' },
-        { name: 'friend #3', key: '3' },
-        { name: 'friend #4', key: '4' },
-        { name: 'friend #5', key: '5' },
-        { name: 'friend #6', key: '6' },
-
+        { name: 'friend #1' },
+        { name: 'friend #2' },
+        { name: 'friend #3' },
+        { name: 'friend #4' },
+        { name: 'friend #5' },
+        { name: 'friend #6' },
+        { name: 'friend #7' },
+        { name: 'friend #8' },
+        { name: 'friend #9' },
+        { name: 'friend #10' },
+        { name: 'friend #11' },
+        { name: 'friend #12' },
     ]
 
     return (
         <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={(friend) => friend.name}
             data={friends}
             renderItem={({ item }) => {
-                return <Text>{item.name}</Text>;
+                return <Text style={style.textStyle}>{item.name}</Text>;
             }}
         />
     );
 }
 
-const style = StyleSheet.create({});
+const style = StyleSheet.create({
+    textStyle: {
+        marginVertical: 50
+    }
+});
 
 export default ListScreen;
