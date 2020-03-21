b83960133cec1a741b5a6e5e18d3e74b69445f35
Aadhi <aadhi@fasteningcode.com>
2020-03-21 14:50:56 +1300

    List Screen method 1

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
 const ListScreen = () => {
+    const friends = [
+        { name: 'friend #1', key: '1' },
+        { name: 'friend #2', key: '2' },
+        { name: 'friend #3', key: '3' },
+        { name: 'friend #4', key: '4' },
+        { name: 'friend #5', key: '5' },
+        { name: 'friend #6', key: '6' },
+
+    ]
+
     return (
-        <Text>
-            List Screen
-        </Text>
+        <FlatList
+            data={friends}
+            renderItem={({ item }) => {
+                return <Text>{item.name}</Text>;
+            }}
+        />
     );
 }
 
