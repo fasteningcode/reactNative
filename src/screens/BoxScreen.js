0352884d13249c6ec4b0b4b8b9267a33a1326ceb
Aadhi <aadhi@fasteningcode.com>
2020-03-22 13:24:23 +1300

    styles

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,10 +4,11 @@ import { View, Text, StyleSheet } from 'react-native'
 const BoxScreen = () => {
    return (
       <View style={styles.viewStyle}>
-         <Text style={styles.textStyle}> RT</Text>
-         <Text style={styles.textStyle}> RT</Text>
-         <Text style={styles.textStyle}> RT</Text>
-
+         <Text style={styles.textStyle}> Text Style 1</Text>
+         <Text style={styles.textTwoStyle}> Text Style 2</Text>
+         <Text style={styles.textThreeStyle}> Text Style 3</Text>
+         {/* <Text style={styles.textStyle}> Text Style 4</Text>
+         <Text style={styles.textTwoStyle}> Text Style 2</Text> */}
       </View>
    )
 }
@@ -16,13 +17,24 @@ const styles = StyleSheet.create({
    viewStyle: {
       borderWidth: 3,
       borderColor: 'black',
+      alignItems: 'stretch',
+      // flexDirection: 'row',
+      height: 200,
+      // justifyContent: 'center'
    },
-   textStyle: {
-      borderWidth: 10,
+   textOneStyle: {
+      borderWidth: 3,
       borderColor: 'red',
-      margin: 20,
-
-
+   },
+   textTwoStyle: {
+      borderWidth: 3,
+      borderColor: 'red',
+      fontSize: 18,
+      left: 10
+   },
+   textThreeStyle: {
+      borderWidth: 3,
+      borderColor: 'red',
    }
 });
 
